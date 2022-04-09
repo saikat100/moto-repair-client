da8ff880f8448e9386fa4d0f5831d619396be8aa
user <user@users-MacBook-Pro-2.local>
2022-04-09 01:20:11 -0700

    add firebase and img

---
+++ src/Components/LoginAuth/LoginConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCu19iNsLzk2QzSqrFcVUzxZfRZ4x6JXTA",
+	authDomain: "max-bike-repair.firebaseapp.com",
+	projectId: "max-bike-repair",
+	storageBucket: "max-bike-repair.appspot.com",
+	messagingSenderId: "825240662908",
+	appId: "1:825240662908:web:6538b675f28c6a9d8daaf9",
+};
+export default firebaseConfig;
